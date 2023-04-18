96367a337f3a7f8626ed2578a79bed6a5e57517d
Lucas <larsonlucas112@gmail.com>
2023-04-18 16:57:34 -0500

    Can comment and load comments

--- anonboard/src/COMPONENTS/Header.js
+++ anonboard/src/COMPONENTS/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {getDatabase, ref, set} from "firebase/database"
+import {getDatabase, ref, set, onValue} from "firebase/database"
 import {signInWithGoogle} from "../firebase"
+
 
 
 const startComment = () => {
@@ -8,17 +9,67 @@ const startComment = () => {
 }
 
 
+export const fetchComments = () => {
+  document.getElementById('comment-container').style.display = "none"
+    const topicName = String(document.getElementById('current-topic').getAttribute('value'))
+    const db = getDatabase();
+
+    const infoRef = ref(db, topicName);
+    onValue(infoRef, (snapshot) => {
+            const messageContainer = document.getElementById('message-container')
+            messageContainer.innerHTML = ''
+            const data = snapshot.val();  
+            let keys = Object.keys(data);
+            const updated = keys.map( key => {
+              return{
+                key, ...data[key]
+              }
+            })          
+            for( let i = 0; i < updated.length; i++){
+              const newMessage = document.createElement('p')
+              newMessage.setAttribute('id', 'message')
+              newMessage.innerHTML = updated[i].comment.text
+              console.log(updated.parent)
+              messageContainer.prepend(newMessage)
+            } 
+    })
+    document.getElementById("text-input").value = ""
+}
+
+
 const makeComment = () => {
     document.getElementById('comment-container').style.display = "none"
     let text = document.getElementById('text-input').value
-    const time = new Date();
-    console.log(time)
+    const topicName = String(document.getElementById('current-topic').getAttribute('value'))
     const db = getDatabase();
-    const reference = ref(db, "What do you think about dogs?/" + new Date())
+    const reference = ref(db, `${topicName}/` + new Date())
+
 
     set(reference, {
       comment: {text: text,
                 }
+    })
+
+    const infoRef = ref(db, topicName);
+    onValue(infoRef, (snapshot) => {
+            const messageContainer = document.getElementById('message-container')
+            messageContainer.innerHTML = ''
+            const data = snapshot.val();  
+            let keys = Object.keys(data);
+            const updated = keys.map( key => {
+              return{
+                key, ...data[key]
+              }
+            })
+            
+            for( let i = 0; i < updated.length; i++){
+              const newMessage = document.createElement('p')
+              newMessage.setAttribute('id', 'message')
+              newMessage.innerHTML = updated[i].comment.text
+              console.log(updated.parent)
+              messageContainer.prepend(newMessage)
+            }   
+            
     })
     document.getElementById("text-input").value = ""
 }
